75fb37a8493413de8556413772fe9ec2aa1b2cbe
Andrew Crosby <imarcrosby@optimum.net>
2020-10-29 13:03:22 -0400

    Added password length input and error if not within the requirements

--- Develop/script.js
+++ Develop/script.js
@@ -14,10 +14,17 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 // create data for character sets ; array or string
-var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-var alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var specialChar = ["!", "#", "$", "%", "&", "(", ")", "+", "*", ",", "/", "@", "?"]
+numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+specialChar = ["!", "#", "$", "%", "&", "(", ")", "+", "*", ",", "/", "@", "?"]
 // ask user length of password
+var passwordLength = prompt("How many character would you like your password between 8 - 128");
+
+ if (Number(passwordLength < 8) + (passwordLength > 128)) {
+  alert ("please select a number BETWEEN 8 and 128")
+}
+
+
 // check the length meeting minimum requirement 
 // confirm user for character sets ; uppercase lowercase special and numbers
 // each confirm store to variable
